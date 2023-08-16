31067959d23808ad393a17705dee90f8fbe62fe1
爱学习的打工人 <124147251+GitLuckyRan@users.noreply.github.com>
2023-08-16 18:46:16 +0800

    Update ex1.m

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -128,7 +128,7 @@ xlabel('\theta_0'); ylabel('\theta_1');
 % Contour plot
 figure;
 % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))           %logspace(a,b,n) 在 10 的幂 10^a 和 10^b（10 的 N 次幂）之间生成 n 个点 contour(x,y,z,[...])生成值为[...]的等值线
 xlabel('\theta_0'); ylabel('\theta_1');
 hold on;
 plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
