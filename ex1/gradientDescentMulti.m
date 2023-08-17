348dbccc63b7b36c5976523f680ea2ca7ad4cd08
爱学习的打工人 <124147251+GitLuckyRan@users.noreply.github.com>
2023-08-17 19:08:26 +0800

    Update gradientDescentMulti.m

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -17,7 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
+ S = (1 / m) * (X' * (X * theta - y));
+    theta = theta - alpha .* S;
 
 
 
