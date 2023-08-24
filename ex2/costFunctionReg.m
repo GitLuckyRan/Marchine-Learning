bbd593ebbcad1f7852ed1f2753ff006030003f9b
爱学习的打工人 <124147251+GitLuckyRan@users.noreply.github.com>
2023-08-24 10:30:35 +0800

    Update costFunctionReg.m

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -16,12 +16,12 @@ grad = zeros(size(theta));%初始化梯度，是J的偏导数
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-J=-1./m*(y'*log(sigmoid(X*theta)+(1-y)'*log(1-sigmoid(X*theta))))+lambda./(2*m)*theta'*theta;
+J=-1./m*(y'*log(sigmoid(X*theta))+(1-y)'*log(1-sigmoid(X*theta)))+lambda./(2*m)*theta'*theta;
 grad=1./m*X'*(sigmoid(X*theta)-y)+lambda./m*theta;
-
-
-
-
 % =============================================================
-
+%==========================================优化=======================================
+%[J, grad] = costFunction(theta, X, y);
+%J = J + lambda/(2*m)*(sum(theta.^2) - theta(1).^2);    %因为θ0对函数的欠拟合和过拟合没什么影响，所以默认不对θ0做惩罚
+%grad = grad + lambda/m*theta;
+%grad(1) = grad(1) - lambda/m*theta(1);    %不对θ0做出惩罚所以相对其它θ的导数要区别处理
 end
